08d8586ad3081987885df4693bc5c5333afb349b
Cesar Almeida <cesaralmeida89@gmail.com>
2023-09-03 12:11:50 +0200

    add data persistence with localStorage

--- src/App.js
+++ src/App.js
@@ -6,17 +6,35 @@ import { TodoList } from './components/TodoList'
 
 import './App.css'
 
-const TODOS = [
-  { text: 'primer todo', completed: true },
-  { text: 'segundo todo', completed: true },
-  { text: 'tercer todo', completed: false },
-  { text: 'cuarto todo', completed: false },
-  { text: 'quinto todo', completed: true }
-]
+// const TODOS = [
+//   { text: 'primer todo', completed: true },
+//   { text: 'segundo todo', completed: true },
+//   { text: 'tercer todo', completed: false },
+//   { text: 'cuarto todo', completed: false },
+//   { text: 'quinto todo', completed: true }
+// ]
+
+// localStorage.setItem('TODOS_V1', JSON.stringify(TODOS))
 
 function App () {
-  const [todos, setTodos] = useState(TODOS)
+  const localStorageTODOS = localStorage.getItem('TODOS_V1')
+  let parsedTODOS = []
+
+  if (!localStorageTODOS) {
+    parsedTODOS = []
+    localStorage.setItem('TODOS_V1', JSON.stringify([]))
+  } else {
+    parsedTODOS = JSON.parse(localStorageTODOS)
+  }
+
+  const [todos, setTodos] = useState(parsedTODOS)
   const [searchValue, setSearchValue] = useState('')
+
+  function saveTodos (newTodos) {
+    const stringifiedTODOS = JSON.stringify(newTodos)
+    localStorage.setItem('TODOS_V1', stringifiedTODOS)
+    setTodos(newTodos)
+  }
 
   return (
     <div className='App'>
@@ -26,7 +44,7 @@ function App () {
       <main>
         <TodoCounter todos={todos} />
         <TodoSearch searchValue={searchValue} onSearchValueChange={setSearchValue} />
-        <TodoList todos={todos} onTodosChange={setTodos} searchValue={searchValue} />
+        <TodoList todos={todos} onTodosChange={saveTodos} searchValue={searchValue} />
       </main>
     </div>
   )
